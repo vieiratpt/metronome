d560b28a1704fd1f1f0b0e6e725d4d150066468c
João Vieira <vieiratpt@pm.me>
2020-09-04 00:24:44 +0100

    main.cpp: Clean useless code.

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@
 int main(int argc, char *argv[]) {
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    Metronome metronome();
-
     qmlRegisterSingletonType<Metronome>("Metronome", 1, 0, "Metronome", Metronome::provider);
 
     QGuiApplication app(argc, argv);
